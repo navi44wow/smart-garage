6dc8fea8bbff0be2246f7bf9666b6c371ed8566d
iliyan02 <iliyan02@abv.bg>
2023-04-09 08:54:13 +0300

    Add Security configuration

--- src/main/java/com/example/smartgarage/config/SecurityConfig.java
+++ src/main/java/com/example/smartgarage/config/SecurityConfig.java
@@ -13,9 +13,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
     private final SmartGarageUserService userDetailsService;
@@ -23,7 +30,6 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     public SecurityConfig(SmartGarageUserService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -32,41 +38,9 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.
-                authorizeRequests().
-                // allow access to static resources to anyone
-                        requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
-                // allow access to index, user login and registration to anyone
-                        antMatchers("/", "/users/login", "/users/register").permitAll().
-                // protect all other pages
-                        antMatchers("/**").authenticated().
-                and().
-                // configure login with HTML form
-                        formLogin().
-                // our login page will be served by the controller with mapping /users/login
-                        loginPage("/users/login").
-                // the name of the username input field in OUR login form is username (optional)
-                        usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
-                // the name of the user password input field in OUR login form is password (optional)
-                        passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
-                // on login success redirect here
-                        defaultSuccessUrl("/home").
-                // on login failure redirect here
-                        failureForwardUrl("/users/login-error").
-                and().
-                logout().
-                // which endpoint performs logout, e.g. http://localhost:8080/logout (!this should be POST request)
-                        logoutUrl("/logout").
-                // where to land after logout
-                        logoutSuccessUrl("/").
-                // remove the session from the server
-                        invalidateHttpSession(true).
-                // delete the session cookie
-                        deleteCookies("JSESSIONID");//bye! :-)
+    protected void configure(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable();
     }
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.
