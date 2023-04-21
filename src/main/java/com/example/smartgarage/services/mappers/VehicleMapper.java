09eb69f68e6f3726685d00e7a09c0e859e6b125b
User <ivan.belev69@gmail.com>
2023-04-21 17:52:05 +0300

    adding VehicleMapper.java

---
+++ src/main/java/com/example/smartgarage/services/mappers/VehicleMapper.java
@@ -0,0 +1,43 @@
+//package com.example.smartgarage.services.mappers;
+//
+//
+//import com.example.smartgarage.models.dtos.VehicleDto;
+//import com.example.smartgarage.models.entities.Vehicle;
+//import com.example.smartgarage.repositories.VehicleRepository;
+//import com.example.smartgarage.services.contracts.ModelService;
+//import com.example.smartgarage.services.contracts.UserService;
+//import com.example.smartgarage.services.contracts.VehicleService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class VehicleMapper {
+//
+//    private final VehicleRepository vehicleRepository;
+//    private final VehicleService vehicleService;
+//    private final UserService userService;
+//    private final ModelService modelService;
+//
+//    @Autowired
+//    public VehicleMapper(VehicleRepository vehicleRepository, VehicleService vehicleService, UserService userService, ModelService modelService) {
+//        this.vehicleRepository = vehicleRepository;
+//        this.vehicleService = vehicleService;
+//        this.userService = userService;
+//        this.modelService = modelService;
+//    }
+//
+//    public Vehicle createDtoToObject(VehicleDto vehicleDto) {
+//        Vehicle vehicle = new Vehicle();
+//        //Vehicle vehicle = vehicleService.getById(vehicleDto.getve)
+//        //User user = userService.getById(vehicleDto.getUser());
+//        //Model model = modelService.getById(vehicleDto.getModel());
+//
+//        vehicle.setUser(userService.getById(vehicleDto.getUser()));
+//        vehicle.setCarModelId(modelService.getById(vehicleDto.getModel()));
+//
+//        vehicle.setVIN(vehicleDto.getVIN());
+//        vehicle.setCreationYear(vehicleDto.getCreationYear());
+//        vehicle.setLicensePlate(vehicleDto.getLicensePlate());
+//        return vehicle;
+//    }
+//}
